de3599558fab844f14fee9249e4acd4d7fb088df
Youssef Gaber <ygabrola@gmail.com>
2015-08-07 07:16:46 +0200

    No need to install dev vendors.

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -3,7 +3,7 @@
 @task('deploy', ['on' => 'production'])
 cd /var/www/savemyga.me
 git pull origin master
-composer install
+composer install --no-dev
 php artisan migrate --force
 php artisan route:cache
 php artisan config:cache
