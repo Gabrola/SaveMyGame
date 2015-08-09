e98b22c294141bc762d8c63c06f23a05b6867bd4
Youssef Gaber <ygabrola@gmail.com>
2015-08-09 23:59:06 +0200

    Log summoner names

--- app/SummonerSearch.php
+++ app/SummonerSearch.php
@@ -20,8 +20,6 @@ class SummonerSearch
 
         $internalName = \LeagueHelper::getInternalName($summonerName);
 
-        \Log::info($internalName);
-
         $summoner = Summoner::byInternalName($region, $internalName)->first();
 
         if(!is_null($summoner))
