a6ca33177cbb9b214710a918c0f6484c33f35d4c
Youssef Gaber <ygabrola@gmail.com>
2015-08-11 01:51:52 +0200

    Use concurrent connections to check for summoners

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -85,25 +85,78 @@ class PageController extends Controller
         ]);
     }
 
+    public function multiRequest($data, $options = array()) {
+
+        // array of curl handles
+        $curly = array();
+        // data to be returned
+        $result = array();
+
+        // multi handle
+        $mh = curl_multi_init();
+
+        // loop through $data and create curl handles
+        // then add them to the multi-handle
+        foreach ($data as $id => $d) {
+
+            $curly[$id] = curl_init();
+
+            $url = (is_array($d) && !empty($d['url'])) ? $d['url'] : $d;
+            curl_setopt($curly[$id], CURLOPT_URL,            $url);
+            curl_setopt($curly[$id], CURLOPT_HEADER,         0);
+            curl_setopt($curly[$id], CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curly[$id], CURLOPT_SSL_VERIFYPEER, false);
+
+            // post?
+            if (is_array($d)) {
+                if (!empty($d['post'])) {
+                    curl_setopt($curly[$id], CURLOPT_POST,       1);
+                    curl_setopt($curly[$id], CURLOPT_POSTFIELDS, $d['post']);
+                }
+            }
+
+            // extra options?
+            if (!empty($options)) {
+                curl_setopt_array($curly[$id], $options);
+            }
+
+            curl_multi_add_handle($mh, $curly[$id]);
+        }
+
+        // execute the handles
+        $running = null;
+        do {
+            curl_multi_exec($mh, $running);
+        } while($running > 0);
+
+
+        // get content and remove handles
+        foreach($curly as $id => $c) {
+            $result[$id] = curl_multi_getcontent($c);
+            curl_multi_remove_handle($mh, $c);
+        }
+
+        // all done
+        curl_multi_close($mh);
+
+        return $result;
+    }
+
     public function test()
     {
         try {
             $monitoredUsers = MonitoredUser::whereConfirmed(true)->whereRegion('OCE')->limit(10)->get();
 
             // Initiate each request but do not block
-            $promises = [];
-
-            $client = new Client(['base_uri' => 'https://oce.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/OC1/']);
-
-            $startTime = microtime(true);
+            $data = [];
 
             /** @var MonitoredUser $monitoredUser */
             foreach ($monitoredUsers as $monitoredUser)
-                $promises[$monitoredUser->id] = $client->getAsync($monitoredUser->summoner_id . '?api_key=' . env('RIOT_API_KEY'));
+                $data[] = 'https://oce.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/OC1/' . $monitoredUser->summoner_id . '?api_key=' . env('RIOT_API_KEY');
 
-            try {
-                $results = Promise\unwrap($promises);
-            } catch (\Exception $e){}
+            $startTime = microtime(true);
+
+            $this->multiRequest($data);
 
             $commandTime = microtime(true) - $startTime;
 
