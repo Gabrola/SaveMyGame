e59f9238767b464e047b283a976af8450acff15b
Youssef Gaber <ygabrola@gmail.com>
2015-08-15 01:22:48 +0200

    Quicker migration

--- app/Console/Commands/CleanMatches.php
+++ app/Console/Commands/CleanMatches.php
@@ -38,23 +38,40 @@ class CleanMatches extends Command
      */
     public function handle()
     {
+        DB::raw('SET unique_checks=0');
+        DB::raw('SET foreign_key_checks=0');
+
         $this->comment('Migrating chunks');
 
         $this->output->progressStart(DB::table('chunks')->count());
 
-        DB::table('chunks')->select(['id', 'chunk_data'])->chunk(100, function($chunks) {
+        DB::table('chunks')->chunk(200, function($chunks) {
+            DB::beginTransaction();
+
             foreach($chunks as $chunk) {
+                $chunkInsertionId = DB::table('chunks_tmp')->insertGetId([
+                    'db_game_id'    =>  $chunk->db_game_id,
+                    'platform_id'   =>  $chunk->platform_id,
+                    'game_id'       =>  $chunk->game_id,
+                    'chunk_id'      =>  $chunk->chunk_id,
+                    'keyframe_id'   =>  $chunk->keyframe_id,
+                    'next_chunk_id' =>  $chunk->next_chunk_id,
+                    'duration'      =>  $chunk->duration
+                ]);
+
                 $id = DB::table('chunk_data')->insertGetId([
-                    'chunk_id'      => $chunk->id,
+                    'chunk_id'      => $chunkInsertionId,
                     'chunk_data'    => $chunk->chunk_data
                 ]);
 
-                DB::table('chunks')->where('id', $chunk->id)->update([
+                DB::table('chunks_tmp')->where('id', $chunkInsertionId)->update([
                     'chunk_data_id' => $id
                 ]);
             }
 
-            $this->output->progressAdvance(100);
+            DB::commit();
+
+            $this->output->progressAdvance(200);
         });
 
         $this->output->progressFinish();
@@ -63,21 +80,35 @@ class CleanMatches extends Command
 
         $this->output->progressStart(DB::table('keyframes')->count());
 
-        DB::table('keyframes')->select(['id', 'keyframe_data'])->chunk(100, function($keyframes) {
+        DB::table('keyframes')->select(['id', 'keyframe_data'])->chunk(200, function($keyframes) {
+            DB::beginTransaction();
+
             foreach($keyframes as $keyframe) {
+                $keyframeInsertionId = DB::table('keyframes_tmp')->insertGetId([
+                    'db_game_id'    =>  $keyframe->db_game_id,
+                    'platform_id'   =>  $keyframe->platform_id,
+                    'game_id'       =>  $keyframe->game_id,
+                    'keyframe_id'   =>  $keyframe->keyframe_id,
+                ]);
+
                 $id = DB::table('keyframe_data')->insertGetId([
-                    'keyframe_id'      => $keyframe->id,
+                    'keyframe_id'      => $keyframeInsertionId,
                     'keyframe_data'    => $keyframe->keyframe_data
                 ]);
 
-                DB::table('keyframes')->where('id', $keyframe->id)->update([
+                DB::table('keyframes_tmp')->where('id', $keyframeInsertionId)->update([
                     'keyframe_data_id' => $id
                 ]);
             }
 
-            $this->output->progressAdvance(100);
+            DB::commit();
+
+            $this->output->progressAdvance(200);
         });
 
         $this->output->progressFinish();
+
+        DB::raw('SET unique_checks=1');
+        DB::raw('SET foreign_key_checks=1');
     }
 }
