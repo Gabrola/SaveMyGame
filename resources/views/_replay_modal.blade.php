4c1da6595a9107c459044bb39a7881a87a575d1e
Youssef Gaber <ygabrola@gmail.com>
2015-08-17 07:38:29 +0200

    Add messages and FAQs

--- resources/views/_replay_modal.blade.php
+++ resources/views/_replay_modal.blade.php
@@ -5,6 +5,9 @@
             <div class="collapsible-header">Windows Command</div>
             <div class="collapsible-body modal-collapse-body">
                 <p>Open a command prompt, paste this into it and press enter. Make sure your League of Legends client is running.</p>
+                @if($windowsCommandId == 'windowsCommandPartial')
+                    <p><strong>This is a one-use command. Re-open this dialog to retrieve a new command.</strong></p>
+                @endif
                 <div style="display: flex">
                     <textarea id="{{ $windowsCommandId }}" rows="1" readonly class="command-area" onclick="this.focus();this.select()">{{ $windowsCommand }}</textarea>
                     <i class="mdi-content-content-copy copy-button" data-copy-element="{{ $windowsCommandId }}" data-zclip-path="{{ asset('build/js/ZeroClipboard.swf') }}"></i>
@@ -21,6 +24,9 @@
             <div class="collapsible-header">Mac Command</div>
             <div class="collapsible-body modal-collapse-body">
                 <p>Open a terminal window, paste this into it and press enter. Make sure your League of Legends client is running.</p>
+                @if($macCommandId == 'macCommandPartial')
+                    <p><strong>This is a one-use command. Re-open this dialog to retrieve a new command.</strong></p>
+                @endif
                 <div style="display: flex">
                     <textarea id="{{ $macCommandId }}" rows="1" readonly class="command-area" onclick="this.focus();this.select()">{{ $macCommand }}</textarea>
                     <i class="mdi-content-content-copy copy-button" data-copy-element="{{ $macCommandId }}" data-zclip-path="{{ asset('build/js/ZeroClipboard.swf') }}"></i>
--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -24,5 +24,15 @@
                     regardless of how much time has passed. This is done because replays take up too much space on our servers, in addition to replays almost always not working on later patches of the game.
                     Game stats will be saved forever though, only the replay data will be deleted.</p></div>
         </li>
+        <li>
+            <div class="collapsible-header">Why does my LoL client go black after I watch a replay?</div>
+            <div class="collapsible-body"><p>Unfortunately, this is something the client does when any replay is played and this is out of our control. However, this doesn't happen if you are on
+                the post-game stats screen, so you may make want to watch your replays right after you finish your game.</p></div>
+        </li>
+        <li>
+            <div class="collapsible-header">What are partial replays?</div>
+            <div class="collapsible-body"><p>Partial replays allow you to watch just the moments you choose from a list of game events. This should save time from when you used search for that epic
+                blind 360 no scope 420 blaze it baron steal in a 1-hour replay.</p></div>
+        </li>
     </ul>
 @endsection
