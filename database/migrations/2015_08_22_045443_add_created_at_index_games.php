5b2048efec23ddbc0f0d36e195d8913707498165
Youssef Gaber <ygabrola@gmail.com>
2015-08-22 06:55:48 +0200

    Add created at index to games

---
+++ database/migrations/2015_08_22_045443_add_created_at_index_games.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCreatedAtIndexGames extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->index(['created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropIndex(['created_at']);
+        });
+    }
+}
