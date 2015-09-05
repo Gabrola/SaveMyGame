d43c0422a0b767b29ceca70c58c69e67c49b7507
Youssef Gaber <ygabrola@gmail.com>
2015-09-06 00:14:12 +0200

    Delete directly

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -88,7 +88,7 @@ class Game extends \Eloquent
 
     public function deleteReplay()
     {
-        \File::deleteDirectory(\LeagueHelper::getReplayDirectory($this->platform_id, $this->game_id));
+        system('rm -rf ' . escapeshellarg(\LeagueHelper::getReplayDirectory($this->platform_id, $this->game_id)));
         $this->status = 'deleted';
         $this->save();
     }
